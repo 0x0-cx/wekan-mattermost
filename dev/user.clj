925b71c4de7ac3aac831166ead6e986889c49a51
Vlad Bokov <razum2um@mail.ru>
2019-12-13 15:29:09 +0300

    Start helpers

---
+++ dev/user.clj
@@ -0,0 +1,15 @@
+(ns user
+  (:require [nrepl.server :as nrepl]
+            [clojure.tools.namespace.repl :refer [refresh]]
+            [wekan-mattermost.core :as core]))
+
+(def http-server (atom nil))
+
+(defn reset []
+  (when @http-server (.stop @http-server))
+
+  (refresh)
+
+  ;; TODO: use integrant system & make http & nrepl a part of it
+  (reset! http-server (core/start-http-server {:port 3000}))
+  (defonce nrepl-server (nrepl/start-server :port 7888)))
