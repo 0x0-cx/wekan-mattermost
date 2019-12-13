481ab3417c42138773d9d6e476fc86441a1a9603
Vlad Bokov <razum2um@mail.ru>
2019-12-13 14:40:38 +0300

    http server

--- src/wekan_mattermost/core.clj
+++ src/wekan_mattermost/core.clj
@@ -1,9 +1,14 @@
-(ns wekan-mattermost.core)
+(ns wekan-mattermost.core
+  (:require [ring.adapter.jetty :refer [run-jetty]]
+            [jsonista.core :as j]))
 
-(defn inc-1 [a]
-  (let [x 4]
-    (+ a x)))
+(defn app [req]
+  (println (pr-str {:req req}))
+  {:status 200 :headers {} :body (j/write-value-as-string {:text "hello clojure"})})
 
+(defn start-http-server [{:keys [port]}]
+  (let [port (Integer. (or (System/getenv "port") port))]
+    (run-jetty #'app {:port port :join? false})))
 
 (defn -main [& args]
-  (println "ok"))
+  (start-http-server {:port 3000}))
