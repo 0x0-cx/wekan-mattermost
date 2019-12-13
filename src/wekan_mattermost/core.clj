05c1ba47d1ce8e9615d8f05f989b8d69421befbc
Vlad Bokov <razum2um@mail.ru>
2019-12-13 15:28:53 +0300

    Echo server

--- src/wekan_mattermost/core.clj
+++ src/wekan_mattermost/core.clj
@@ -1,10 +1,16 @@
 (ns wekan-mattermost.core
   (:require [ring.adapter.jetty :refer [run-jetty]]
+            [ring.util.response :refer [response]]
             [jsonista.core :as j]))
 
+;; TODO
+(def mapper (j/object-mapper {:encode-key-fn name :decode-key-fn keyword}))
+
 (defn app [req]
-  (println (pr-str {:req req}))
-  {:status 200 :headers {} :body (j/write-value-as-string {:text "hello clojure"})})
+  (println (pr-str req))
+  (if (-> req :request-method (= :post))
+    (response (j/write-value-as-string (-> req :body j/read-value)))
+    (response "Please send POST with wekan wenhook: https://github.com/wekan/wekan/wiki/Webhook-data")))
 
 (defn start-http-server [{:keys [port]}]
   (let [port (Integer. (or (System/getenv "port") port))]
